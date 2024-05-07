d40271c5ad5b8101f291dda9fc112248722a69f3
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-05-07 07:56:46 +0530

    🌱 Remove types enum, add types array

--- src/glyphs.h
+++ src/glyphs.h
@@ -6,19 +6,10 @@
 
 /* GLYPHS */
 
-enum types
-{
-  JAVASCRIPT,
-  C,
-  CPP,
-  CSHARP,
-  HTML,
-  CSS,
-  TEXT,
-  MARKDOWN,
-  PYTHON,
-  UNKNOWN
-};
+char *types[] = {
+    "js", "c", "cpp", "cs", "html", "css", "txt", "md", "py"};
+
+int extensions = 9;
 
 wchar_t glyphs[MAX] = {0xe781, 0xf0671, 0xf0672, 0xf031b, 0xe736, 0xe749, 0xf09a8, 0xe73e, 0xe73c, 0xf15b};
 
