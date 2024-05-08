53e5df3b0064500dd3a84d4d228e821a14e9aa8a
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-05-08 20:16:13 +0530

    🌱 Parse arguments for -a, display all files

--- src/main.c
+++ src/main.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <wchar.h>
 #include <locale.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "list.h"
+#include "args.h"
 
 int main(int argc, char *argv[])
 {
   setlocale(LC_CTYPE, "");
 
+  OPTIONS options;
+
+  options.displayAll = false;
+  options.directory = malloc(sizeof(argc) * 1000);
+
+  strcpy(options.directory, ".");
+
   if (argc == 1)
-    listDirectory(".");
+    listDirectory(&options);
   else
-    listDirectory(argv[1]);
+  {
+    getArgs(argc, argv, &options);
+    listDirectory(&options);
+  }
 
   return 0;
 }
