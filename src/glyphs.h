32c6167637dbb59c7513a6e3f6ff575de2b88ab6
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-05-08 20:14:33 +0530

    🌱 Add glyphs for various file types

--- src/glyphs.h
+++ src/glyphs.h
@@ -7,10 +7,10 @@
 /* GLYPHS */
 
 char *types[] = {
-    "js", "c", "cpp", "cs", "html", "css", "txt", "md", "py"};
+    "js", "c", "cpp", "cs", "html", "css", "txt", "md", "py", "pdf", "png", "jpg", "jpeg", "csv", "folder"};
 
-int extensions = 9;
+int extensions = 15;
 
-wchar_t glyphs[MAX] = {0xe781, 0xf0671, 0xf0672, 0xf031b, 0xe736, 0xe749, 0xf09a8, 0xe73e, 0xe73c, 0xf15b};
+wchar_t glyphs[MAX] = {0xe781, 0xf0671, 0xf0672, 0xf031b, 0xe736, 0xe749, 0xf09a8, 0xe73e, 0xe73c, 0xf0226, 0xf0e2d, 0xf0225, 0xf0225, 0xeefc, 0xf024b, 0xf15b};
 
 #endif
